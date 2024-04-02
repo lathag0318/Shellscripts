96e5027584594338ef1bba8087117efe479835d7
lathag0318 <lathag0318@gmail.com>
2024-04-02 12:19:38 -0700

    files added

---
+++ division.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Please Enter a number for a:" a
+read -p "Please Enter a number for b:" b
+
+div=$(($a/$b))
+
+echo "The division is: $div"
---
+++ sub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+num1=30
+num2=14
+sub=$(($num1-$num2))
+echo subtraction of $num1 and $num2 = $sub
